fbc1b26210e1275d56e9065a1064d8bb99376855
Abdullah Javaid <cheema1140@gmail.com>
2021-09-29 10:18:23 -0700

    file commited

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learn.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
